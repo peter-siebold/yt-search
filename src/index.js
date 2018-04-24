eeac5c82687d44e848a31cbf6ddc52c27e4ceba4
peter-himself <peter.siebold@icloud.com>
2018-04-24 14:58:02 +0200

    use api key from config file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
-
+import API_KEY from "./config/api_key";
 
 class App extends Component {
     constructor(props){
